067bfe32637f5bb59f9bca3cbc84257f2cddb355
Aren <aren.musayelyan555@gmail.com>
2023-07-15 20:22:05 +0400

    Initial Commit

---
+++ PhraseMaster/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PhraseMaster
+//
+//  Created by Aren Musayelyan on 15.07.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("Hello World")
+    }
+    
+    
+    
+}
+
